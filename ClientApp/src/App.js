c48a82af0d520d754273c423ac9e67afc0d0befb
vijay <pandey.vijay4@gmail.com>
2022-09-01 23:13:18 -0400

    Adding basicstyle and compponents

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Menu from "./components/menu/Menu";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Menu />
+        <Routes>
+          <Route
+            path="/"
+            element={<div className="main">test App</div>}
+          ></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
